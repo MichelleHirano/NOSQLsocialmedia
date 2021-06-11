c0bbef287386680d4371d86cb7fa2941eb996555
MHirano <michelle@hiranohomes.com>
2021-06-11 15:18:31 -0700

    Thought Schema

--- models/Thought.js
+++ models/Thought.js
@@ -29,3 +29,39 @@ const ReactionSchema = new Schema(
         }
     }
 )
+
+const ThoughtSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim:true
+        },
+        thoughtText: {
+            type: String,
+            required: "Please enter thoguhts in form of text",
+            maxlength:280
+        },
+        createdAt:{
+            type:Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+        id: false
+    }
+)
+
+ThoughtSchema.virtual("reactionCount").get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model ('Thought', ThoughtSchema)
+
+module.exports = Thought

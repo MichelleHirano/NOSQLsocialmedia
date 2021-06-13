dd0422f2f2edaa048b992ca422ce357b4fdbc8c9
MHirano <michelle@hiranohomes.com>
2021-06-13 15:12:28 -0700

    update and delete user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -37,4 +37,51 @@ const userController ={
         console.log("BODY OBJECT", body)
         User.create(body)
             .then(dbUserData => res.json(dbUserData))
-    }
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+    updateUser({ params, body}, res){
+        User.findOneAndUpdate({ _id: params.id }, body, {
+            new: true,
+            runValidators: true,
+          })
+            .then((dbUserData) => {
+              if (!dbUserData) {
+                res.status(404).json({ message: "No user found with this id!" });
+                return;
+              }
+              res.json(dbUserData);
+            })
+            .catch((err) => res.status(400).json(err));
+     },
+    deleteUser({ params}, res){
+        User.findOneAndDelete({ _id: params.id})
+        .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id!" });
+              return;
+            }
+        User.updateMany(
+            { _id: {$in: dbUserData.frinds} },
+            { $pull: { friends: params.id} }
+        ) 
+        .then(() => {
+            Thought.deleteMany({ username: dbUserData.username})
+            .then(() => {
+                res.json ({ message: "Successfully deleted user"});
+            })
+            .catch ((err) => res.status (400).json(err));
+        })
+        .catch ((err) => res.status (400).json(err));    
+    })
+    .catch ((err) => res.status (400).json(err));
+    },
+
+
+
+    
+    
+    
+    module.exports =userController
